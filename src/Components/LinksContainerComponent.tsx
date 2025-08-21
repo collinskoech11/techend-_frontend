2e29bf134839dd6532bc9caa355d7e82bf81fabf
collinskoech11 <collinskoechck34@gmail.com>
2025-08-21 15:54:27 +0300

    feat(ui): Add loading indicators for AuthDialog and FAQ components using Suspense

--- src/Components/LinksContainerComponent.tsx
+++ src/Components/LinksContainerComponent.tsx
@@ -14,6 +14,7 @@ import {
   ListItemIcon,
   Tooltip,
   Badge,
+  CircularProgress,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
@@ -285,7 +286,9 @@ const LinksContainerComponent = forwardRef((props: any, ref: any) => {
               </Menu>
             </Box>
           ) : (
-            <AuthDialog onTrigger={refetchUser} />
+            <Suspense fallback={<CircularProgress size={24} />}>
+              <AuthDialog onTrigger={refetchUser} />
+            </Suspense>
           )}
         </Box>
         {/* --- END: Desktop Icons --- */}
