3b51f5ab358a4b1eaf6f1d3cf27fdc248e6f3b58
collinskoech11 <collinskoechck34@gmail.com>
2025-11-04 18:34:36 +0300

    buid fixes

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import {
   ProductItemStyled,
   ProductImage,
--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import React, { useState } from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import MuiLink from "@mui/material/Link";
